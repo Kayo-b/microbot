1d1048cb0a67f3754307a79cba38d2cc2b2bc795
Kayo-b <kyo.bhz@gmail.com>
2025-07-14 10:28:14 +0200

    Setting examplePlugin back to original form.

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/example/ExamplePlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/example/ExamplePlugin.java
@@ -8,12 +8,6 @@ import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 import net.runelite.client.ui.overlay.OverlayManager;
-import net.runelite.client.plugins.microbot.Microbot;
-import net.runelite.client.plugins.microbot.pluginscheduler.api.SchedulablePlugin;
-import net.runelite.client.plugins.microbot.pluginscheduler.api.SchedulerCoordinates;
-import net.runelite.client.plugins.microbot.pluginscheduler.event.PluginScheduleEntrySoftStopEvent;
-import net.runelite.api.coords.WorldPoint;
-import net.runelite.client.plugins.microbot.util.walker.Rs2Walker;
 
 import javax.inject.Inject;
 import java.awt.*;
@@ -25,72 +19,36 @@ import java.awt.*;
         enabledByDefault = false
 )
 @Slf4j
-public class ExamplePlugin extends Plugin implements SchedulablePlugin, SchedulerCoordinates {
+public class ExamplePlugin extends Plugin {
     @Inject
     private ExampleConfig config;
-    
-    private boolean hasSchedulerCoordinates = false;
-    private int schedulerX, schedulerY, schedulerZ;
-    private boolean scriptStarted = false; 
     @Provides
     ExampleConfig provideConfig(ConfigManager configManager) {
         return configManager.getConfig(ExampleConfig.class);
     }
 
-    // @Inject
-    // private OverlayManager overlayManager;
-    // @Inject
-    // private ExampleOverlay exampleOverlay;
+    @Inject
+    private OverlayManager overlayManager;
+    @Inject
+    private ExampleOverlay exampleOverlay;
 
     @Inject
     ExampleScript exampleScript;
 
-    @Subscribe
-    @Override
-    public void onPluginScheduleEntrySoftStopEvent(PluginScheduleEntrySoftStopEvent event) {
-        if (event.getPlugin() == this) {
-            // Cleanup operations
-            Microbot.getClientThread().invokeLater(() -> {
-                Microbot.stopPlugin(this);
-            });
-        }
-    }
 
     @Override
     protected void startUp() throws AWTException {
-        // if (overlayManager != null) {
-        //     overlayManager.add(exampleOverlay);
-        //     exampleOverlay.myButton.hookMouseListener();
-        // }
-        
-        // Use a delay to allow scheduler to set coordinates if this is a scheduled start
-        // For manual activation, coordinates won't be set, so script starts immediately
-        Microbot.getClientThread().invokeLater(() -> {
-            // Give scheduler 1 game tick to set coordinates if this is a scheduled start
-            if (!scriptStarted && !hasSchedulerCoordinates) {
-                startScript();
-            }
-        });
+        if (overlayManager != null) {
+            overlayManager.add(exampleOverlay);
+            exampleOverlay.myButton.hookMouseListener();
+        }
+        exampleScript.run(config);
     }
 
     protected void shutDown() {
         exampleScript.shutdown();
-        clearSchedulerCoordinates(); // Clear coordinates on shutdown
-        scriptStarted = false; // Reset script started flag
-        // overlayManager.remove(exampleOverlay);
-        // exampleOverlay.myButton.unhookMouseListener();
-    }
-    
-    /**
-     * Start the script - called either immediately (manual start) or after coordinates are set (scheduler start)
-     */
-    public synchronized void startScript() {
-        if (!scriptStarted && !exampleScript.isRunning()) {
-            Microbot.log("Starting ExampleScript with coordinates source: " + 
-                (hasSchedulerCoordinates ? "scheduler" : "config"));
-            exampleScript.run(this);
-            scriptStarted = true;
-        }
+        overlayManager.remove(exampleOverlay);
+        exampleOverlay.myButton.unhookMouseListener();
     }
     int ticks = 10;
     @Subscribe
@@ -104,44 +62,6 @@ public class ExamplePlugin extends Plugin implements SchedulablePlugin, Schedule
             ticks = 10;
         }
 
-    }
-
-    @Override
-    public void setSchedulerCoordinates(int x, int y, int z) {
-        this.schedulerX = x;
-        this.schedulerY = y;
-        this.schedulerZ = z;
-        this.hasSchedulerCoordinates = true;
-        Microbot.log("Scheduler coordinates set: " + x + ", " + y + ", " + z);
-        
-        startScript();
-    }
-    
-    @Override
-    public boolean hasSchedulerCoordinates() {
-        return hasSchedulerCoordinates;
-    }
-    
-    @Override
-    public void clearSchedulerCoordinates() {
-        hasSchedulerCoordinates = false;
-        schedulerX = schedulerY = schedulerZ = 0;
-        // Don't reset scriptStarted here as the script might still be running with config coordinates
-    }
-    
-    @Override
-    public int getEffectiveX() {
-        return hasSchedulerCoordinates ? schedulerX : config.walkX();
-    }
-    
-    @Override
-    public int getEffectiveY() {
-        return hasSchedulerCoordinates ? schedulerY : config.walkY();
-    }
-    
-    @Override
-    public int getEffectiveZ() {
-        return hasSchedulerCoordinates ? schedulerZ : config.walkZ();
     }
 
 }
--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/example/ExampleScript.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/example/ExampleScript.java
@@ -1,35 +1,23 @@
 package net.runelite.client.plugins.microbot.example;
 
-import net.runelite.api.coords.WorldPoint;
 import net.runelite.client.plugins.microbot.Microbot;
 import net.runelite.client.plugins.microbot.Script;
-import net.runelite.client.plugins.microbot.util.walker.Rs2Walker;
 
 import java.util.concurrent.TimeUnit;
 
 public class ExampleScript extends Script {
-    public static double version = 1.0;
-    private boolean hasWalkedToConfig = false;
+
     public static boolean test = false;
-    
-    private ExamplePlugin plugin; 
 
-    public boolean run(ExamplePlugin plugin) { 
-        this.plugin = plugin;
+    public boolean run(ExampleConfig config) {
         Microbot.enableAutoRunOn = false;
-
-        hasWalkedToConfig = false;
         mainScheduledFuture = scheduledExecutorService.scheduleWithFixedDelay(() -> {
             try {
                 if (!Microbot.isLoggedIn()) return;
                 if (!super.run()) return;
                 long startTime = System.currentTimeMillis();
-                Microbot.log("has walked to config: " + hasWalkedToConfig);
+
                 // CODE HERE
-                if (!hasWalkedToConfig) {
-                    walkToConfigLocation();
-                    hasWalkedToConfig = true;
-                }
                 Microbot.log("Hello World from ExampleScript!");
                 
                 long endTime = System.currentTimeMillis();
@@ -43,28 +31,8 @@ public class ExampleScript extends Script {
         return true;
     }
     
-    private void walkToConfigLocation() {
-        // Use plugin effective coordinates (scheduler overrides config)
-        WorldPoint destination = new WorldPoint(
-            plugin.getEffectiveX(), 
-            plugin.getEffectiveY(), 
-            plugin.getEffectiveZ()
-        );
-        
-        String source = plugin.hasSchedulerCoordinates() ? "scheduler" : "config";
-        Microbot.log("Using " + source + " coordinates: " + destination.getX() + ", " + 
-                     destination.getY() + ", " + destination.getPlane());
-        
-        if (destination.getX() != 0 || destination.getY() != 0 || destination.getPlane() != 0) {
-            Rs2Walker.walkTo(destination);
-            Microbot.log("Walking to " + source + " location: " + destination);
-        }
-    }
     @Override
     public void shutdown() {
         super.shutdown();
-        hasWalkedToConfig = false;
-        Rs2Walker.setTarget(null);
-        Microbot.log("ExampleScript shutdown");
     }
 }
